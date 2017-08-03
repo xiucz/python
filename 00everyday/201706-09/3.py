2c0c8243a85485a7a707d32f52f62f7ff317c7cd
xiucz <xiucz@users.noreply.github.com>
2017-08-03 13:36:36 +0800

    Update 3.py

--- 00everyday/201706-09/3.py
+++ 00everyday/201706-09/3.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 import re
+def Prokaryotic_gff2gtf(gff_file):
+    with open(gff_file) as gfffile:
+        for line in gfffile:
+            if line.startswith("#"):continue
+            else:
+                cols = line.strip().split("\t")
+                if cols[2] == "gene":
+                   core_item = cols[8].split(";")
+                   if "locus_tag=" in core_item[-1] and "old_locus_tag=" not in core_item[-1]:
+                       locus_tag = re.search(';locus_tag=(.*)', cols[8]).group(1)
+                   else:
+                       locus_tag = re.search(';locus_tag=(.*?);', cols[8]).group(1)
+                   print("\t".join(cols[0:8]) + "\t" + 'gene_id "' + locus_tag + '"; transcript_id "' + locus_tag + '";')
+                   print("\t".join(cols[0:2]) + "\t" + "exon" + "\t" + "\t".join(cols[3:8]) + "\t" +'gene_id "' + \
+                         locus_tag + '"; transcript_id "' + locus_tag + '";')
+                else:continue
+Prokaryotic_gff2gtf("GCF_000162295.1_ASM16229v1_genomic.gff")
+
 def fillgtf(ori_gtffile):
     with open(ori_gtffile) as myhd:
         for line in myhd.readlines():
             cols = line.strip().split("\t")
-            if "=true" in line:
-                locus_tag = re.search(';locus_tag=(.*?);', cols[8]).group(1)
-            else:
+            core_item = cols[8].split(";")
+            if "locus_tag=" in core_item[-1] and "old_locus_tag=" not in core_item[-1]:
                 locus_tag = re.search(';locus_tag=(.*)', cols[8]).group(1)
+            else:
+                locus_tag = re.search(';locus_tag=(.*?);', cols[8]).group(1)
             print("\t".join(cols[0:8]) + "\t" + 'gene_id "' + locus_tag + '"; transcript_id "' + locus_tag + '";')
-
-
-fillgtf("GCF_001287225.1_bcg_Prague_genomic.gtf_ori")
